bc684cbd7547de31d2fb2ab6cca85e87c1c79330
happyNicky <naoldana5@gmail.com>
2026-03-06 16:32:53 +0300

    first commit

---
+++ src/test/java/com/finalyear/liwatch/LiwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalyear.liwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
