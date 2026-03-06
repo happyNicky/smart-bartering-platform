cc8b62d24c256bec151eeb1210276064b7c29399
happyNicky <naoldana5@gmail.com>
2026-03-06 16:32:53 +0300

    first commit

---
+++ src/main/java/com/finalyear/liwatch/LiwatchApplication.java
@@ -0,0 +1,13 @@
+package com.finalyear.liwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiwatchApplication.class, args);
+	}
+
+}
